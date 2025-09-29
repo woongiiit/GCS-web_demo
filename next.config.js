109a6df6c52aa7403a2569407aa790ac8301eda5
W00ngit <woongit1210@naver.com>
2025-09-29 14:05:38 +0900

    fix: enable Next.js standalone output mode

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  output: 'standalone',
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
