a8e294d41c465908f7cf470e2059c63bd2700778
W00ngit <woongit1210@naver.com>
2025-10-24 08:22:14 +0900

    fix: Move licenseKey prop outside init object to resolve TypeScript build error

--- src/components/TinyMCEEditor.tsx
+++ src/components/TinyMCEEditor.tsx
@@ -31,6 +31,7 @@ export default function TinyMCEEditor({
         value={value}
         onEditorChange={handleEditorChange}
         disabled={disabled}
+        licenseKey="gpl"
         init={{
           height: height,
           menubar: false,
@@ -50,8 +51,6 @@ export default function TinyMCEEditor({
           statusbar: false,
           resize: false,
           elementpath: false,
-          // API 키 없이 사용하기 위한 설정
-          license_key: 'gpl',
           setup: (editor) => {
             editor.on('init', () => {
               editor.getContainer().style.border = '1px solid #d1d5db'
